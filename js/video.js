6bfe482337ed7edbbd46b41fef022468c5d2093f
danielhanek <97470934+danielhanek@users.noreply.github.com>
2022-04-14 19:22:40 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,4 +1,4 @@
-var video;
+var video = document.querySelector("#player1")
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
@@ -11,7 +11,7 @@ window.addEventListener("load", function() {
  document.querySelector("#play").addEventListener("click", function() {
  	console.log("Play Video");
 	 video.play()
-	 console.log("The current volume is " + video.volume)
+	 console.log("The current volume is " + video.volume*100 + "%")
 	 document.querySelector("#volume").innerHTML = video.volume *100+"%"
 	
 });
@@ -23,19 +23,19 @@ document.querySelector("#pause").addEventListener("click", function() {
 
 
 document.querySelector("#slower").addEventListener("click", function() {
-	console.log("Current speed is" + video.playbackRate)
+	console.log("Current speed is " + video.playbackRate)
 	video.playbackRate = video.playbackRate * .95
-	console.log("New speed is" + video.playbackRate)
+	console.log("New speed is " + video.playbackRate)
 });
 
 document.querySelector("#faster").addEventListener("click", function() {
-	console.log("Current speed is" + video.playbackRate)
+	console.log("Current speed is " + video.playbackRate)
 	video.playbackRate = video.playbackRate / .95
-	console.log("New speed is" + video.playbackRate)
+	console.log("New speed is " + video.playbackRate)
 });
 
 document.querySelector("#mute").addEventListener("click", function() {
-	console.log("The value of mute is" + video.muted)
+	console.log("The value of mute is " + video.muted)
 	if (video.muted === false) {    
 		video.muted = true;
 	}
@@ -45,18 +45,33 @@ document.querySelector("#mute").addEventListener("click", function() {
 	
 });
 
-document.querySelector("#slider").addEventListener("click", function() {
-	console.log(this.value)
+document.querySelector("#slider").addEventListener("change", function() {
+	console.log("The current volume is " + this.value +"%")
+	video.volume = this.value / 100;
 	
 });
 
 
-document.getElementById("vintage").addEventListener( 'click', function(){
+document.querySelector("#vintage").addEventListener( 'click', function(){
 	console.log("Class changed to Old School")
 	document.getElementById("player1").className = "oldSchool";
 });
 
-document.getElementById("orig").addEventListener( 'click', function(){
-	console.log("Class changed to Old School")
+document.querySelector("#orig").addEventListener( 'click', function(){
+	console.log("Class changed back to original")
 	document.getElementById("player1").className = "video";
 });
+
+document.querySelector("#slider").addEventListener("change",function(){
+	sliderVolume = document.getElementById("volume");
+	sliderVolume.innerHTML = this.value + "%";
+});
+
+document.querySelector("#skip").addEventListener("click",function(){
+	console.log("Current time of video is " + video.currentTime +"sec")
+	video.currentTime = video.currentTime + 15;
+
+	video.addEventListener('ended', function() {
+		video.currentTime = 0;
+	}
+)});
